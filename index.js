4ce9124835ffabdf02db288348567bc2fdb3d909
mitchell <mitchel2206@gmail.com>
2022-07-14 23:12:27 -0300

    dolmar

---
+++ index.js
@@ -0,0 +1,123 @@
+
+/*function palabra  ( perfume ) {
+
+    for (let i = 0; i >= perfume; i++){
+     if ( i === perfume){
+        console.log(`son siete digitos`)
+     } else if ( i >= perfume){
+        console.log(`es agua`)
+     }
+    } 
+    
+    }
+
+ palabra.perfume()
+
+class Clientes {
+    constructor(nombre, apellido, edad, domicilio, codigoPostal, ciudad, pais, estadoCivil) {
+        this.nombre = nombre
+            this.apellido = apellido
+            this.edad = edad
+            this.domicilio = domicilio
+            this.codigoPostal = codigoPostal
+            this.ciudad = ciudad
+            this.pais = pais
+            this.estadoCivil = estadoCivil
+    }
+
+   mostrandoClientes () {
+    console.log (cliente1)
+}  }
+
+const cliente1 = new Clientes ("Raul", "Davila", 28, "Lobos1254", 4547, "caracas", "Venezuela", "Viudo" )
+const cliente2 = new Clientes("Juan" , "Suarez", 23, "Belgrano 224", "4000", "tucuman", "argentina" , "soltero")
+const cliente3 = new Clientes ("Juan", "Sánchez", 19, "Rosales", 231, "Caballito", "Argentina", "Casado")
+const cliente4 = new Clientes ("maria", "Pereyra",26, "bucardo 142", 5221, "cordoba", "argentina", "casada")
+const cliente5 = new Clientes ("Emilio", "Ansaldi", 35, "Córdoba 1234", "Santa Fe", "Argentina", "Casado")
+
+
+cliente1.mostrandoClientes()*/
+
+
+/*function palabra (perfume) {
+
+    for (let i = 0; i >= perfume; i++){
+     if ( i === perfume){
+        console.log(`son siete digitos`)
+     } else if ( i <= perfume){
+        console.log(i)
+     }
+    } 
+    
+    }
+
+ palabra.perfume()*/
+
+
+/*function perfume  (burberry) {
+
+    for( let i = 0; i <= burberry; i++){
+
+        if (i === burberry){
+        
+        console.log(“son siete dígitos”)
+        
+        }
+    }
+}
+
+console.log(perfume)
+
+
+
+for( let i = 0; i <= burberry; i++){
+
+if (i === burberry){
+
+console.log(“son siete dígitos”)
+
+}
+}
+
+burberry()*/
+
+
+
+
+var perfume = "burberry"
+var digito = perfume.length
+var marcas = "channel"
+console.log(digito)
+
+
+for (let i = 0; i < perfume.length; i++) {
+
+    if (perfume.length >= marcas.length) {
+        console.log("tiene ocho caracteres")
+    } else {
+        console.log(" no tiene ocho caracteres")
+    }
+
+}
+
+
+const expensas = function (cuota, cuota2) {
+
+    for (let i = 0; i <= cuota; i++) {
+        if ( cuota >= cuota2){
+            console.log("el max de cuota son 20, tenemos un error asi que te sale gratis")
+        } else if (cuota <= cuota2) {
+        console.log(`El precio correspondiente: ${cuota2}`)
+
+        }
+            
+    }
+
+}
+
+expensas(19,20)
+
+
+
+
+
